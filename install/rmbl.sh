5f007b1da92e3ea474c6ed0ba5d8839db4c965bc
RMBL-VPN <136141445+RMBL-VPN@users.noreply.github.com>
2023-08-09 06:01:02 +0800

    Update rmbl.sh

--- install/rmbl.sh
+++ install/rmbl.sh
@@ -3,13 +3,13 @@
 MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
 
-DOMAIN=rmblvpn.xyz
+DOMAIN=yogzvpn.cloud
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 #subsl=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-dns=${sub}.rmblvpn.xyz
+dns=${sub}.yogzvpn.cloud
 #NS_dns=slowdns-${subsl}.kuhing.my.id
-CF_ID=kadeknovi148@gmail.com
-CF_KEY=0acb39278825f08e3ceaba41c1169cde
+CF_ID=ysshvpn@gmail.com
+CF_KEY=0d626234700bad388d6d07b49c42901445d1c
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${dns}..."
